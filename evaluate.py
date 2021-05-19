evaluation loss reporting wrong value

evaluate.py
@@ -6,10 +6,13 @@ from dataset import HSAFingertipDataset
 import torchvision
 import tqdm
 import argparse
+import numpy as np
 
 import torch.nn.functional as F
 
 DEVICE = torch.device("cuda:1")
+
+
 def evaluate(checkpoint_dir, data_working_dir):
     # load model
     model = SimpleCNN().to(DEVICE)
@@ -34,18 +37,28 @@ def evaluate(checkpoint_dir, data_working_dir):
                        val=val_dataloader,
                        test=test_dataloader)
 
-    tq_obj = tqdm.tqdm(dataloaders['test'])
-    model.eval()  # Set model to evaluate mode
-    for batch_idx, sample_batched in enumerate(tq_obj):
-        position_predictions = model(sample_batched['image'].to(DEVICE))
+    for phase in ['train', 'val', 'test']:
+        tq_obj = tqdm.tqdm(dataloaders['test'])
+        model.eval()  # Set model to evaluate mode
 
-        input_ = position_predictions
-        target = sample_batched['label']['pos'].float().to(DEVICE)
+        total_samples = len(tq_obj)
+        total_sum_vec = np.array([0., 0., 0.])[None, :]
 
-        assert input_.shape == target.shape
+        for batch_idx, sample_batched in enumerate(tq_obj):
+            position_predictions = model(sample_batched['image'].to(DEVICE))
 
-        loss = F.mse_loss(input_, target, reduction='mean')
-        tq_obj.set_description(f"Test loss {loss}")
+            input_ = position_predictions
+            target = sample_batched['label']['pos'].float().to(DEVICE)
+
+            assert input_.shape == target.shape
+
+            loss = F.mse_loss(input_, target, reduction='mean')
+
+            summed_loss = loss.data.cpu().numpy().sum(axis=0)
+            total_sum_vec += summed_loss
+            tq_obj.set_description(f"{phase} loss {summed_loss}")
+
+        print(f"{phase} loss aggregate {total_sum_vec / total_samples}")
 
 
 if __name__ == "__main__":
@@ -55,5 +68,5 @@ if __name__ == "__main__":
     parser.add_argument('--num_epochs', type=int, default=50)
     parser.add_argument('--checkpoint_freq', type=int, help="Checkpoint frequency in epochs", default=10)
     args = parser.parse_args()
-    data_working_dir = "/home/richard/Dropbox (MIT)/6.869 Project/data"
+    data_working_dir = "/home/richard/data/hsa_data"
     evaluate(args.checkpoint_dir, data_working_dir)

train.py
@@ -16,9 +16,9 @@ DEVICE = torch.device("cuda:1")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', type=int, default=32)
-parser.add_argument('--num_epochs', type=int, default=50)
-parser.add_argument('--checkpoint_freq', type=int, help="Checkpoint frequency in epochs", default=1)
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--num_epochs', type=int, default=100)
+parser.add_argument('--checkpoint_freq', type=int, help="Checkpoint frequency in epochs", default=10)
 args = parser.parse_args()
 pd.set_option('display.max_colwidth', None)
 
@@ -45,7 +45,8 @@ model = SimpleCNN()
 model = model.to(DEVICE)
 
 optimizer = torch.optim.Adam(model.parameters())
-wandb.init(project="6869_final_project")
+wandb.init(project="richards_runs",
+           entity="6869_final_project_team")
 
 # this is where you log hyperparameters
 # for now, just make sure the git ID and string of the model class are logged
@@ -83,10 +84,11 @@ for epoch in range(args.num_epochs):
             if phase == 'train':
                 loss.backward()
                 optimizer.step()
+
+        wandb.log({f"{phase} loss": loss})
         print(f"ln62: {phase} loss {loss}")
         print("\n")
 
     if epoch % args.checkpoint_freq == 0:
         torch.save(model.state_dict(), os.path.join(wandb.run.dir, f"checkpoint{epoch}"))
 
-    wandb.log(dict(loss=loss))